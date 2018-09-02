9a8b12d348849ed97eb3b4f8b86904106d0d30a3
RahulRagesh <rahulragesh@yahoo.co.in>
2018-09-02 16:29:10 +0530

    Create solution.cpp

---
+++ Steve Halim/UVa 01124 - Celebrity Jeopardy/solution.cpp
@@ -0,0 +1,12 @@
+//https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=3565
+#include <iostream>
+using namespace std;
+
+int main() {
+	char str[10000000];
+    int flag = 1;
+	while(scanf ("%[^\n]%*c", str)!=EOF){
+	    printf("%s\n",str);   
+	}
+	return 0;
+}
